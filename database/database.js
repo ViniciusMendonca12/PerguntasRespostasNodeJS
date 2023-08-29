777f45a6fab2fa5a3393f68c0e6a81de36934ed1
ViniciusMendonca12 <94076366+ViniciusMendonca12@users.noreply.github.com>
2023-08-28 23:29:16 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize('guiaperguntas','root','familia100',{// nome do banco, usuario e senha
+const connection = new Sequelize('xxxxxx','root','xxxxx',{// nome do banco, usuario e senha
     host:'localhost',// servidor onde esta rodando
     dialect: 'mysql'// qual tipo de banco de dados
 })
